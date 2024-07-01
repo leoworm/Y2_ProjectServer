8717abc36f9cfc975a75649a40187c5e9c5f1352
n0sferatu <leo.charles.p@gmail.com>
2024-07-01 20:39:17 +0200

    Taking first elem from array

--- lib/projectserver/users.ex
+++ lib/projectserver/users.ex
@@ -27,7 +27,7 @@ defmodule Projectserver.Users do
     Logger.debug(Repo.exists?(query))
 
     if Repo.exists?(query) do
-      entry = Repo.all(query)
+      entry = Repo.all(query)[0]
 
       if Map.has_key?(entry, :password) do
         if entry.password == password do
